d88eae7dbaba755cd92d60116b0718666aad15da
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-12 18:56:55 +0100

    done

--- partSeven.py
+++ partSeven.py
@@ -8,31 +8,28 @@ while con == "y":
     opp = input("What operation would you like to perform? ")
     num2 = float(input("What's the second number? "))
 
-    if opp == "/" and num2 == "0":
+    if opp == "+":
+        result = num1 + num2 
+        print (f"{num1} + {num2} = {result}")
+    elif opp == "-":
+        result = num1 - num2
+        print (f"{num1} - {num2} = {result}")
+    elif opp == "*":
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif num1 == "0" or num2 == "0":
         print("Error")
-    else:
-        if opp == "+":
-            result = num1 + num2 
-            print (f"{num1} + {num2} = {result}")
-        elif opp == "-":
-            result = num1 - num2
-            print (f"{num1} - {num2} = {result}")
-        elif opp == "*":
-            result = num1 * num2
-            print(f"{num1} * {num2} = {result}")
-        elif num1 == "0" or num2 == "0":
-            print("Error")
-        elif opp == "/":
-            if num2 > 0:
-                result = num1 / num2
-                print(f"{num1} / {num2} = {result}")
-            else:
-                print("ERROR")
-        elif opp == "%":
-            result = num1 % num2
-            print(f"{num1} % {num2} = {result}")
+    elif opp == "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
         else:
             print("ERROR")
+    elif opp == "%":
+        result = num1 % num2
+        print(f"{num1} % {num2} = {result}")
+    else:
+        print("ERROR")
     
     q = input("Would you like to restart? (y/n) ")
     if q == "n":
