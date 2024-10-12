561f1f1774ed7081161ce6d49b2964637657e9ae
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-12 16:02:46 +0100

    done

--- partSix.py
+++ partSix.py
@@ -1,1 +1,9 @@
+age = int(input("What's your age? "))
+student = str(input("Are you a student? Please answer yes or no. "))
 
+if age < 12 or age >= 65:
+    print("£5 please")
+elif student == "yes":
+    print("£8 please")
+else:
+    print("£10 please")
