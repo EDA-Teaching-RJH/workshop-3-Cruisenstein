3aa78ae61840fdc5a588aa933ab2f4d1832cd27d
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-12 16:49:52 +0100

    test

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,17 @@
+num1 = float(input("What's the first number?"))
+opp = input("What operation would you like to perform?")
+num2 = float(input("What's the second number?"))
+
+if opp = "+":
+    result = num1 + num2 
+    print (f"{num1} + {num2} = {result}")
+elif "-":
+    result_ = num1 - num2
+    print (f"{num1} - {num2} = {result_}")
+else:
+    print("HI")
+    
+
+
+
 
