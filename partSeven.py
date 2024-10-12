92d84ad8f7a350f18edd77395a3d54a374c179e6
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-12 18:12:59 +0100

    exit added

--- partSeven.py
+++ partSeven.py
@@ -1,28 +1,38 @@
-num1 = float(input("What's the first number?"))
-opp = input("What operation would you like to perform?")
-num2 = float(input("What's the second number?"))
+con = input("Would you like to start? ")
 
-if opp == "+":
-    result = num1 + num2 
-    print (f"{num1} + {num2} = {result}")
-elif opp == "-":
-    result = num1 - num2
-    print (f"{num1} - {num2} = {result}")
-elif opp == "*":
-    result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
-elif opp == "/":
+while con == "yes":
+
+    num1 = float(input("What's the first number? "))
+    opp = input("What operation would you like to perform? ")
+    num2 = float(input("What's the second number? "))
+
+    if opp == "+":
+        result = num1 + num2 
+        print (f"{num1} + {num2} = {result}")
+    elif opp == "-":
+        result = num1 - num2
+        print (f"{num1} - {num2} = {result}")
+    elif opp == "*":
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif opp == "/":
         if num1 == "0" or num2 == "0":
              print("no")
         else:
             result = num1 / num2
             print(f"{num1} / {num2} = {result}")
-elif opp == "^":
-    result = num1 ** num2
-    print(f"{num1} ^ {num2} = {result}")
-elif opp == "%":
-    result = num1 % num2
-    print(f"{num1} % {num2} = {result}")
+    elif opp == "^":
+        result = num1 ** num2
+        print(f"{num1} ^ {num2} = {result}")
+    elif opp == "%":
+        result = num1 % num2
+        print(f"{num1} % {num2} = {result}")
+    elif opp == "quit" or num1 == "quit" or num2 == "quit":
+        print("Quitting the apllication")
+        break
+    else:
+        print("ERROR")
+    print("NEW EQUATION")
 
 
 
