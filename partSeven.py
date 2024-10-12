ef7b58b44a38529042df3e38629e748d58266c16
Cruisenstein <Cruisenstein00@gmail.com>
2024-10-12 16:50:40 +0100

    commit

--- partSeven.py
+++ partSeven.py
@@ -2,15 +2,29 @@ num1 = float(input("What's the first number?"))
 opp = input("What operation would you like to perform?")
 num2 = float(input("What's the second number?"))
 
-if opp = "+":
+if opp == "+":
     result = num1 + num2 
     print (f"{num1} + {num2} = {result}")
-elif "-":
-    result_ = num1 - num2
-    print (f"{num1} - {num2} = {result_}")
-else:
-    print("HI")
-    
+elif opp == "-":
+    result = num1 - num2
+    print (f"{num1} - {num2} = {result}")
+elif opp == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif opp == "/":
+        if num1 == "0" or num2 == "0":
+             print("no")
+        else:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+elif opp == "^":
+    result = num1 ** num2
+    print(f"{num1} ^ {num2} = {result}")
+elif opp == "%":
+    result = num1 % num2
+    print(f"{num1} % {num2} = {result}")
+
+
 
 
 
